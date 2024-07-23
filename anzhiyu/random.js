f91b16b0f13fac305ad9c07378aa08b5c18d1c64
smilesLN <1498879944@qq.com>
2024-07-23 16:26:00 +0800

    Site updated: 2024-07-23 16:25:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/01/FRW知识库/","2021/04/01/KMS激活服务器搭建/","2021/04/01/windows更新服务器搭建/","2024/04/01/网站搭建/"];function toRandomPost(){
+var posts=["2024/04/01/FRW知识库/","2021/04/01/windows更新服务器搭建/","2024/04/01/网站搭建/","2021/04/01/KMS激活服务器搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
