2524672ea33c5366f9f01c92e90b325b13d74278
smilesLN <1498879944@qq.com>
2024-08-10 16:26:07 +0800

    Site updated: 2024-08-10 16:26:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/01/Day04笔记/","2024/04/01/FRW知识库/","2021/04/01/GQ/","2021/04/01/KMS激活服务器搭建/","2024/04/01/day03笔记/","2024/04/01/网站搭建/","2021/04/01/windows更新服务器搭建/"];function toRandomPost(){
+var posts=["2024/04/01/FRW知识库/","2024/04/01/Day04笔记/","2021/04/01/GQ/","2021/04/01/KMS激活服务器搭建/","2024/04/01/day03笔记/","2024/04/01/网站搭建/","2021/04/01/windows更新服务器搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
